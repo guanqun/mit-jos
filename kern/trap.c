0c8ef759c1ff9bb3ae981b6440cc383505091cab
Guanqun Lu <Guanqun.Lu@Gmail.com>
2008-03-11 20:50:42 +0800

    fault* passed
    Signed-off-by: Guanqun Lu <Guanqun.Lu@Gmail.com>

--- kern/trap.c
+++ kern/trap.c
@@ -148,6 +148,13 @@ trap_dispatch(struct Trapframe *tf)
 {
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
+	switch (tf->tf_trapno) {
+	case T_PGFLT:
+		page_fault_handler(tf);
+		break;
+	default:
+		break;
+	}
 
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);

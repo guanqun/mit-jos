48424f448bf464cd4308c3094e97900d55d9361c
Guanqun Lu <guanqun.lu@gmail.com>
2008-05-01 16:02:41 +0800

    spawn implemented.
    Signed-off-by: Guanqun Lu <guanqun.lu@gmail.com>

--- lib/spawn.c
+++ lib/spawn.c
@@ -7,6 +7,7 @@
 
 // Helper functions for spawn.
 static int init_stack(envid_t child, const char **argv, uintptr_t *init_esp);
+static int load_elf_to_child(int fd, struct Proghdr *ph, envid_t child);
 
 // Spawn a child process from a program image loaded from the file system.
 // prog: the pathname of the program to run.
@@ -16,18 +17,36 @@ static int init_stack(envid_t child, const char **argv, uintptr_t *init_esp);
 int
 spawn(const char *prog, const char **argv)
 {
-	unsigned char elf_buf[512];
 	struct Trapframe child_tf;
 	envid_t child;
+	struct Elf elf;
+	struct Proghdr ph;
+	int r, i;
+	int fd;
+	uintptr_t esp;
 
 	// Insert your code, following approximately this procedure:
 	//
 	//   - Open the program file.
+	if ((fd = open(prog, O_RDONLY)) < 0)
+		return fd;
 	//
 	//   - Read the ELF header, as you have before, and sanity check its
 	//     magic number.  (Check out your load_icode!)
+	if (read(fd, (void *)&elf, sizeof(elf)) != sizeof(elf)) {
+		close(fd);
+		return -E_INVAL;
+	}
+	if (elf.e_magic != ELF_MAGIC) {
+		close(fd);
+		return -E_INVAL;
+	}
 	//
 	//   - Use sys_exofork() to create a new environment.
+	if ((child = sys_exofork()) < 0) {
+		close(fd);
+		return child;
+	}
 	//
 	//   - Set child_tf to an initial struct Trapframe for the child.
 	//     Hint: The sys_exofork() system call has already created
@@ -37,6 +56,13 @@ spawn(const char *prog, const char **argv)
 	//
 	//   - Call the init_stack() function above to set up
 	//     the initial stack page for the child environment.
+	if ((r = init_stack(child, argv, &esp)) < 0) {
+		close(fd);
+		return r;
+	}
+	child_tf = envs[ENVX(child)].env_tf;
+	child_tf.tf_eip = elf.e_entry;
+	child_tf.tf_esp = esp;
 	//
 	//   - Map all of the program's segments that are of p_type
 	//     ELF_PROG_LOAD into the new environment's address space.
@@ -75,16 +101,32 @@ spawn(const char *prog, const char **argv)
 	//     The ELF linker does, however, guarantee that no two segments
 	//     will overlap on the same page; and it guarantees that
 	//     PGOFF(ph->p_offset) == PGOFF(ph->p_va).
+	for (i = 0; i < elf.e_phnum; i++) {
+		seek(fd, elf.e_phoff + sizeof(struct Proghdr) * i);
+		read(fd, &ph, sizeof(struct Proghdr));
+
+		if (ph.p_type == ELF_PROG_LOAD) {
+			r = load_elf_to_child(fd, &ph, child);
+			if (r < 0) {
+				cprintf("load elf error: %e", r);
+				close(fd);
+				return r;
+			}
+		}
+	}
+	close(fd);
 	//
 	//   - Call sys_env_set_trapframe(child, &child_tf) to set up the
 	//     correct initial eip and esp values in the child.
+	if ((r = sys_env_set_trapframe(child, &child_tf)) < 0)
+		return r;
 	//
 	//   - Start the child process running with sys_env_set_status().
+	if ((r = sys_env_set_status(child, ENV_RUNNABLE)) < 0)
+		return r;
 
-	// LAB 5: Your code here.
-	
-	(void) child;
-	panic("spawn unimplemented!");
+	// panic("spawn over...\n");
+	return child;
 }
 
 // Spawn, taking command-line arguments array directly on the stack.
@@ -106,9 +148,9 @@ static int
 init_stack(envid_t child, const char **argv, uintptr_t *init_esp)
 {
 	size_t string_size;
-	int argc, i, r;
 	char *string_store;
 	uintptr_t *argv_store;
+	int argc, i, r, len;
 
 	// Count the number of arguments (argc)
 	// and the total amount of space needed for strings (string_size).
@@ -160,22 +202,131 @@ init_stack(envid_t child, const char **argv, uintptr_t *init_esp)
 	//	* Set *init_esp to the initial stack pointer for the child,
 	//	  (Again, use an address valid in the child's environment.)
 	//
-	// LAB 5: Your code here.
-	*init_esp = USTACKTOP;	// Change this!
-
+	for (i = 0; i < argc; i++) {
+		len = strlen(argv[i]);
+		// copy the arguments to string_store
+		memmove(string_store, argv[i], len);
+		// make it null-terminated
+		string_store[len] = 0;
+		// make the right reference
+		argv_store[i] = UTEMP2USTACK(string_store);
+		// advance the string_store pointer
+		string_store += len + 1;
+	}
+	argv_store[argc] = 0;
+	// push 'argv' ptr onto the stack
+	argv_store[-1] = (uintptr_t)UTEMP2USTACK(argv_store);
+	// push 'argc' onto the stack
+	argv_store[-2] = argc;
+	argv_store -= 2;
+	// return the right esp value to the child process
+	*init_esp = UTEMP2USTACK(argv_store);
 	// After completing the stack, map it into the child's address space
 	// and unmap it from ours!
-	if ((r = sys_page_map(0, UTEMP, child, (void*) (USTACKTOP - PGSIZE), PTE_P | PTE_U | PTE_W)) < 0)
-		goto error;
+	if ((r = sys_page_map(0, UTEMP,
+			      child, (void*) (USTACKTOP - PGSIZE),
+			      PTE_P | PTE_U | PTE_W)) < 0) {
+		sys_page_unmap(0, UTEMP);
+		return r;
+	}
 	if ((r = sys_page_unmap(0, UTEMP)) < 0)
-		goto error;
+		return r;
 
+	cprintf("complete init_stack esp: %x.\n", argv_store);
 	return 0;
-
-error:
-	sys_page_unmap(0, UTEMP);
-	return r;
 }
 
+static int load_elf_to_child(int fd, struct Proghdr *ph, envid_t child)
+{
+	uint32_t offset, cur_va;
+	int r, size;
+	void *blk;
 
+	// debug
+	cprintf("proghdr: flag: %d; offset: %x; va: %x; filesz: %x; memsz: %x.\n",
+		ph->p_flags & ELF_PROG_FLAG_WRITE, ph->p_offset,
+		ph->p_va, ph->p_filesz, ph->p_memsz);
+	if (ph->p_flags & ELF_PROG_FLAG_WRITE) {
+		// read/write data here
+		offset = ph->p_offset;
+		cur_va = ph->p_va;
 
+		while (cur_va < ph->p_va + ph->p_filesz) {
+			// calculate 'size', if it exceeds p_filesz,
+			// then, adjust the 'size'.
+			size = PGSIZE - (offset & 0xfff);
+			if (cur_va + size >= ph->p_va + ph->p_filesz)
+				size = ph->p_va + ph->p_filesz - cur_va;
+
+			if ((r = sys_page_alloc(0,
+						(void *)UTEMP,
+						PTE_P |PTE_U |PTE_W)) < 0)
+				return r;
+			if ((r = seek(fd, ROUNDDOWN(offset, PGSIZE))) < 0)
+				return r;
+			if ((r = read(fd,
+				      (void *)UTEMP + (offset & 0xfff),
+				      size)) < 0)
+				return r;
+			if ((r = sys_page_map(0,
+					      (void *)UTEMP,
+					      child,
+					      (void *)ROUNDDOWN(cur_va, PGSIZE),
+					      PTE_P |PTE_U |PTE_W)) < 0)
+				return r;
+			if ((r = sys_page_unmap(0, (void *)UTEMP)) < 0)
+				return r;
+
+			offset += size;
+			cur_va += size;
+		}
+		// zero the region from ph->p_filesz to ph->p_memsz
+		cur_va = ROUNDUP(cur_va, PGSIZE);
+		offset = ROUNDUP(offset, PGSIZE);
+		while (cur_va < ph->p_va + ph->p_memsz) {
+			size = PGSIZE - (offset & 0xfff);
+			if (cur_va + size >= ph->p_va + ph->p_memsz)
+				size = ph->p_va + ph->p_memsz - cur_va;
+
+			if ((r = sys_page_alloc(0,
+						(void *)UTEMP,
+						PTE_P |PTE_U |PTE_W)) < 0)
+				return r;
+
+			// zero them
+			memset((void *)UTEMP + (offset & 0xfff), 0, size);
+
+			if ((r = sys_page_map(0,
+					      (void *)UTEMP,
+					      child,
+					      (void *)ROUNDDOWN(cur_va, PGSIZE),
+					      PTE_P |PTE_U |PTE_W)) < 0)
+				return r;
+			if ((r = sys_page_unmap(0, (void *)UTEMP)) < 0)
+				return r;
+
+			offset += size;
+			cur_va += size;
+		}
+	} else {
+		// text and read-only data
+		offset = ph->p_offset;
+		cur_va = ph->p_va;
+		while (cur_va < ph->p_va + ph->p_filesz) {
+			size = PGSIZE - (offset & 0xfff);
+
+			if ((r = read_map(fd, offset, &blk)) < 0)
+				return r;
+			if ((r = sys_page_map(0,
+					      ROUNDDOWN(blk, PGSIZE),
+					      child,
+					      (void *)ROUNDDOWN(cur_va, PGSIZE),
+					      PTE_P |PTE_U)) < 0)
+				return r;
+
+			offset += size;
+			cur_va += size;
+		}
+	}
+	return 0;
+}
